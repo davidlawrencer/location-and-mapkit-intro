0ef033af40b30271b1ff0894eefff8da8de85487
davidlawrencer <davidlrifkin+github@gmail.com>
2019-11-01 11:55:09 -0400

    Adds location manager with permissions and delegate

--- location-and-mapkit-intro/ViewController.swift
+++ location-and-mapkit-intro/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        requestLocationAndAuthorizeIfNeeded()
+        locationManager.delegate = self
         // Do any additional setup after loading the view.
     }
 
-
+    private func requestLocationAndAuthorizeIfNeeded() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.requestLocation()
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
